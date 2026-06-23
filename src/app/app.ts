4b9eadc8d896cdf850dff16922212434c7ed435f
qadir <aqadir1989@gmail.com>
2026-06-23 23:54:51 +0530

    create-actions-to-call-reducer-and-dispatch-event-from-component-to-update-store

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { decreament, increament, reset } from './store/counter.action';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ngrx-tutorialDemo');
+
+  constructor(private _store: Store) {}
+
+  increament() {
+    this._store.dispatch(increament());
+  }
+  decreament() {
+    this._store.dispatch(decreament());
+  }
+  reset() {
+    this._store.dispatch(reset());
+  }
 }
---
+++ src/app/store/counter.action.ts
@@ -0,0 +1,13 @@
+import { createAction } from '@ngrx/store';
+
+//action to call increament
+
+export const increament = createAction('[] Increament');
+
+//action to call decreament
+
+export const decreament = createAction('[] Decreament');
+
+//action to call reset
+
+export const reset = createAction('[] Reset');
